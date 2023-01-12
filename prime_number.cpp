f39b6386ea2a8a15f12e43cca43c24ff904a5b24
amebaleon <windowleon@gmail.com>
2023-01-12 21:52:13 +0900

    second_2

---
+++ prime_number.cpp
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main(void)
+{
+	unsigned long counter = 0;
+	for (int n = 2; n <= 1000; n++)
+	{
+		int i;
+		for(i = 2; n <= 1000; n++)
+		{
+			int i;
+			for(i = 2; i < n; i++)
+			{
+				counter++;
+				if(n%i==0)
+				break;
+			}
+			if(n == i)
+			printf("%d\n", n);
+		}
+		printf("³ª´°¼ÀÀ» ½ÇÇàÇÑ È½¼ö: %lu\n",counter);
+		return 0;
+	}
+}
